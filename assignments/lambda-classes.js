55f06adba44a49f5f7f18a55ff6e6cc3b983335f
Raudel Flores <rawdell@outlook.com>
2019-10-10 18:23:04 -0500

    finished making the Instructor class. I also tested it out and everything worked out!

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,30 @@ class Person{
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
+
+// let raudel = new Person({name: 'raudel', age: 22, location: 'Chicago'})
+
+class Instructor extends Person {
+    constructor(iAttrs) {
+        super(iAttrs);
+        this.specialty = iAttrs.specialty;
+        this.favLanguage = iAttrs.favLanguage;
+        this.catchPhrase = iAttrs.catchPhrase;
+    }
+
+    demo(subString) {
+        return `Today we are learning about ${subString}`;
+    }
+
+    grade(obj, subj) {
+        return `${obj.name} receives a perfect score on ${subj}`
+    }
+}
+// let pace = new Instructor({
+//     name: 'Pace',
+//     age: 28,
+//     location: 'California',
+//     specialty: 'Front-End Dev',
+//     favLanguage: 'Spanish',
+//     catchPhrase: 'Don\'t forget the homies'
+// })
